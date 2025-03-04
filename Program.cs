f842aa192339b8e8314c6f431e54773356c95405
Opilane TTHK <koroljovamelanie512@gmail.com>
2025-03-04 11:31:37 +0200

    jalgrattas

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            //küsi kasutajalt numbrit
+            Console.WriteLine("sisesta number");
+            string kasutajaSisend = Console.ReadLine();
+
+            if (int.TryParse(kasutajaSisend, out int number))
+
+                if (number % 2 = 0)
+
+                    Console.WriteLine($"number {number} on paari.");
+                else
+
+                    Console.WriteLine($"numbr {number} on paaritu.");
+
+            else
+
+                Console.WriteLine("sisestaśite vale numbri!");
+        }
+    }
+}
