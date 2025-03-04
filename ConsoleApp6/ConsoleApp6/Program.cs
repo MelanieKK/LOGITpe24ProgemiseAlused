9286b7ef1ea519f41fb4706a6da337e90f05ae63
Opilane TTHK <opilane@tthk.ee>
2025-03-04 10:45:48 +0200

    age

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta vanus");
+
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 0 && age <= 18)
+
+                Console.WriteLine("oled alaealine.");
+
+            else if (age >= 18 && age <= 64)
+
+                Console.WriteLine("oled täisealine");
+
+            else if (age >= 64 && age <= 98)
+
+                Console.WriteLine("oled pensionäär");
+
+            else
+
+                Console.WriteLine("Vanus on liiga kõrge, et määretleda");
+        }
+    }
+}
