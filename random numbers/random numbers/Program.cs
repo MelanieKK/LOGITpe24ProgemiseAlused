eff6fe47ac9b678a01f6f0127bb5fafb2ee81ffe
MelanieKK <koroljovamelanie512@gmail.com>
2025-03-06 09:07:27 +0200

    taribg

---
+++ random numbers/random numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace random_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int number = new Random().Next(1, 7);
+
+            switch ("number")
+
+                case 1:
+                Console.WriteLine("veeretasid nr " + number);
+                break;
+
+               default:
+                Console.WriteLine("error");
+                break;
+
+
+            
+        }
+    }
+}
